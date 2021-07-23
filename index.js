0b100d8f5f044cab0b7d9dfb3cff594f2d94316c
sdragon0105 <77851434+sdragon0105@users.noreply.github.com>
2021-07-23 19:32:52 +0900

    sitecore server create

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const cors = require('cors');
+
+app.use(cors('*'));
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/signup', (req, res) => {
+    console.log(req.body);
+    res.status(200).json({ message: 'OK' });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
